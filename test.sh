e2a10d9d889c3e230b34262ea80597947ba7df42
Jean-François Wojcieszak <88288940+jeanfrancoiswojcieszak@users.noreply.github.com>
2025-01-11 12:40:55 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-qemu-system-x86_64 -pflash ./bios/bios.bin -m 2G -hda fat:rw:out -netdev bridge,id=hn0,br=br0 -device virtio-net-pci,netdev=hn0 -usb -device usb-ehci,id=usb-ehc -device usb-mouse,bus=usb-bus.0,port=2 -device usb-kbd,bus=usb-bus.0,port=1 -drive "if=pflash,format=raw,file=./bios/vars.bin"
+qemu-system-x86_64 -pflash ./bios/bios.bin -m 2G -hda fat:rw:out -netdev bridge,id=hn0,br=br0 -device virtio-net-pci,netdev=hn0 -usb -device usb-ehci,id=usb-ehc -device usb-mouse,bus=usb-bus.0,port=2 -device usb-kbd,bus=usb-bus.0,port=1 
