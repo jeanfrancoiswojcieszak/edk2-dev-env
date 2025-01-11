acb22dcada644351f8c9289cc8bac494046ba726
Jean-François Wojcieszak <88288940+jeanfrancoiswojcieszak@users.noreply.github.com>
2025-01-11 12:15:36 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run  -it --rm -v ./ProjectFiles:/app/ProjectPkg -v ./out:/app/edk2/Build --name mybuildinginstance edk2-dev-v01
+docker run  -it --rm -v ./bios:/bios -v ./ProjectFiles:/app/ProjectPkg -v ./out:/app/edk2/Build --name mybuildinginstance edk2-dev-v01
